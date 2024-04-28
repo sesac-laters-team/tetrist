c16035dc95fc87463f1576e2a685b65d1475f85f
jjm <jaeminjeon123@gmail.com>
2024-04-28 23:17:07 +0900

    feat: 랭킹창 모달 구현

--- client/src/components/waitingRoom/RankingModalContent.jsx
+++ client/src/components/waitingRoom/RankingModalContent.jsx
@@ -5,18 +5,22 @@ const RankingModalContent = () => {
     { id: 3, username: "user3", points: 2000 },
   ];
 
-  return (<>
-     <div>랭킹 </div>
-    <div className="ranking-list">
-     
-      {rankings.map((rank) => (
-        <div key={rank.id} className={`ranking-item rank-${rank.id}`}>
-          <span className={`medal rank-${rank.id}`}></span>
-          <span className="rank-username">{rank.username}</span>
-          <span className="rank-points">{rank.points}점</span>
+  return (
+    <>
+      <div className="ranking-title-container">
+        <div className="ranking-title">랭킹</div>
+      </div>
+      <div className="ranking-list-container">
+        <div className="ranking-list">
+          {rankings.map((rank) => (
+            <div key={rank.id} className={`ranking-item rank-${rank.id}`}>
+              <span className={`medal rank-${rank.id}`}></span>
+              <span className="rank-username">{rank.username}</span>
+              <span className="rank-points">{rank.points}점</span>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
     </>
   );
 };
