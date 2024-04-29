57bb4f36f1bad282a9ebe202dc46a0c0c1ec8188
yzlybe <yzlybe@gmail.com>
2024-04-29 20:10:28 +0900

    fix: users - 유저 모델 수정 - 임시 DB 데이터 형식 수정

--- server/sql/products.sql
+++ server/sql/products.sql
@@ -3,8 +3,13 @@ select * from products;
 INSERT INTO products (product_id, p_type, p_name, p_img, p_price)
  VALUES (1, "profile", "기본", "default", 0);
 INSERT INTO products (product_id, p_type, p_name, p_img, p_price)
- VALUES (2, "profile", "안경", "glasses", 100);
+ VALUES (2, "profileEdge", "기본", "default", 0);
 INSERT INTO products (product_id, p_type, p_name, p_img, p_price)
  VALUES (3, "theme", "기본", "default", 0);
 INSERT INTO products (product_id, p_type, p_name, p_img, p_price)
- VALUES (4, "theme", "겨울", "winter", 200);
+ VALUES (4, "profile", "프로필A", "profileA", 100);
+INSERT INTO products (product_id, p_type, p_name, p_img, p_price)
+ VALUES (5, "profileEdge", "프로필테두리A", "profileEdgeA", 100);
+
+INSERT INTO products (product_id, p_type, p_name, p_img, p_price)
+ VALUES (6, "theme", "테마A", "themeA", 200);
