e37c4aa5e18f60b8f1194ad7528e8e9b39f49413
yzlybe <yzlybe@gmail.com>
2024-04-29 20:12:31 +0900

    fix: register - 회원가입 시 중복 검사 항목 라우터 분리

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -36,6 +36,66 @@ authRouter.post("/register", usersCtr.postRegister);
 /**
  * @swagger
  * paths:
+ *  /api-server/auth/emailDuplicate:
+ *    post:
+ *      summary: "이메일 중복 검사"
+ *      description: ""
+ *      tags: [User]
+ *      responses:
+ *        "200":
+ *          description: 이메일 사용 가능
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    ok:
+ *                      type: boolean
+ *                    users:
+ *                      type: object
+ *                      example:
+ *                          [
+ *                              {
+    "result": true,
+    "msg": "사용할 수 있는 이메일입니다."
+}
+ *                          ]
+ */
+authRouter.post("/emailDuplicate", usersCtr.emailDuplicate);
+
+/**
+ * @swagger
+ * paths:
+ *  /api-server/auth/nicknameDuplicate:
+ *    post:
+ *      summary: "닉네임 중복 검사"
+ *      description: ""
+ *      tags: [User]
+ *      responses:
+ *        "200":
+ *          description: 닉네임 사용 가능
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    ok:
+ *                      type: boolean
+ *                    users:
+ *                      type: object
+ *                      example:
+ *                          [
+ *                              {
+    "result": true,
+    "msg": "사용할 수 있는 닉네임입니다."
+}
+ *                          ]
+ */
+authRouter.post("/nicknameDuplicate", usersCtr.nickDuplicate);
+
+/**
+ * @swagger
+ * paths:
  *  /api-server/auth/login:
  *    post:
  *      summary: "유저 로그인 요청"
