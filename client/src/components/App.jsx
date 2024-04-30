85df3822a4091ae17b47fc8dc23bcd52dc496b8e
rasarar <1ee_dw@naver.com>
2024-05-01 04:54:29 +0900

    feat: 로그인 페이지 기능 구현 및 예외처리 & 안 쓰는 파일 정리

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
 import GameContainer from "./inGame/GameContainer";
 import WaitingRoomPage from "./page/WaitingRoomPage";
-import MainPage from "./page/MainPage";
 import GamePage from "./page/GamePage";
 import LoginPage from "./page/LoginPage";
 
@@ -13,11 +17,22 @@ function App() {
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
+                    <Route
+                        path="/login"
+                        element={<LoginPage setIsLoggedIn={setIsLoggedIn} />}
+                    />
                     {/* 로그인되어 있지 않은 경우에는 WaitingRoomPage로의 라우팅을 막음 */}
-                    <Route path="/waiting" element={isLoggedIn ? <WaitingRoomPage /> : <Navigate to="/login" />} />
+                    <Route
+                        path="/waiting"
+                        element={
+                            isLoggedIn ? (
+                                <WaitingRoomPage />
+                            ) : (
+                                <Navigate to="/login" />
+                            )
+                        }
+                    />
                     <Route path="/game" element={<GameContainer />} />
-                    <Route path="/main" element={<MainPage />} />
                     <Route path="/tetris" element={<GamePage />} />
                 </Routes>
             </div>
--- client/src/redux/store/module/authModule.js
+++ client/src/redux/store/module/authModule.js
@@ -1,72 +1,78 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Action Types
-const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
-const REGISTER_FAIL = 'auth/REGISTER_FAIL';
-const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
-const LOGIN_FAIL = 'auth/LOGIN_FAIL';
-const LOGOUT = 'auth/LOGOUT';
+const REGISTER_SUCCESS = "auth/REGISTER_SUCCESS";
+const REGISTER_FAIL = "auth/REGISTER_FAIL";
+const LOGIN_SUCCESS = "auth/LOGIN_SUCCESS";
+const LOGIN_FAIL = "auth/LOGIN_FAIL";
+const LOGOUT = "auth/LOGOUT";
 
 // Initial State
 const initialState = {
-  userData: null,
-  error: null
+    userData: null,
+    error: null,
 };
 
 // Reducer
 export default function authReducer(state = initialState, action) {
-  switch (action.type) {
-    case REGISTER_SUCCESS:
-      return {
-        ...state,
-        userData: action.payload,
-        error: null
-      };
-    case REGISTER_FAIL:
-      return {
-        ...state,
-        error: action.payload
-      };
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        userData: action.payload,
-        error: null
-      };
-    case LOGIN_FAIL:
-      return {
-        ...state,
-        error: action.payload
-      };
-    case LOGOUT:
-      return {
-        ...state,
-        userData: null
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case REGISTER_SUCCESS:
+            return {
+                ...state,
+                userData: action.payload,
+                error: null,
+            };
+        case REGISTER_FAIL:
+            return {
+                ...state,
+                error: action.payload,
+            };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                userData: action.payload,
+                error: null,
+            };
+        case LOGIN_FAIL:
+            return {
+                ...state,
+                error: action.payload,
+            };
+        case LOGOUT:
+            return {
+                ...state,
+                userData: null,
+            };
+        default:
+            return state;
+    }
 }
 
 // Action Creators
-export const registerUser = (email, password, nickname) => async dispatch => {
-  try {
-    const response = await axios.post('http://localhost:8080/api-server/auth/register', { email, password, nickname });
-    dispatch({ type: REGISTER_SUCCESS, payload: response.data });
-  } catch (error) {
-    dispatch({ type: REGISTER_FAIL, payload: error.response.data });
-  }
+export const registerUser = (email, password, nickname) => async (dispatch) => {
+    try {
+        const response = await axios.post(
+            "http://localhost:8080/api-server/auth/register",
+            { email, password, nickname }
+        );
+        dispatch({ type: REGISTER_SUCCESS, payload: response.data });
+    } catch (error) {
+        dispatch({ type: REGISTER_FAIL, payload: error.response.data });
+    }
 };
 
-export const loginUser = (email, password) => async dispatch => {
-  try {
-    const response = await axios.post('http://localhost:8080/api-server/auth/login', { email, password });
-    dispatch({ type: LOGIN_SUCCESS, payload: response.data });
-  } catch (error) {
-    dispatch({ type: LOGIN_FAIL, payload: error.response.data });
-  }
+export const loginUser = (email, password) => async (dispatch) => {
+    try {
+        const response = await axios.post(
+            "http://localhost:8080/api-server/auth/login",
+            { email, password }
+        );
+        dispatch({ type: LOGIN_SUCCESS, payload: response.data });
+    } catch (error) {
+        dispatch({ type: LOGIN_FAIL, payload: error.response.data });
+    }
 };
 
 export const logoutUser = () => {
-  return { type: LOGOUT };
+    return { type: LOGOUT };
 };
