15733df38212e625a0022d3bd6c7e5ed1913b8a4
jjm <jaeminjeon123@gmail.com>
2024-05-02 02:09:29 +0900

    fix: 리듀서 상태관리&& 리다이렉트
    usestate로 관리하던것 상태관리&&
    기본페이지 login으로 가게 수정
    /waiting 막기

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,50 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import {
     BrowserRouter as Router,
     Routes,
     Route,
     Navigate,
 } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import GameContainer from "./inGame/GameContainer";
 import WaitingRoomPage from "./page/WaitingRoomPage";
+import LoginPage from "./page/LoginPage";
+import { logoutUser } from "../redux/store/module/authModule";
 import "../styles/CreateRoommodal.scss";
 import "../styles/WaitingRoom.scss";
 import "../styles/menu-button.scss";
 import "../styles/mypagemodal.scss";
 import "../styles/rankingmodal.scss";
 import "../styles/ShopModal.scss";
-import GamePage from "./page/GamePage";
-import LoginPage from "./page/LoginPage";
 import "../styles/pagination.scss";
-import "../styles/font.scss";
+import GamePage from "./page/GamePage";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const isLoggedIn = useSelector((state) => state.auth.userData !== null);
+    const dispatch = useDispatch();
 
-    // 로컬 스토리지에서 로그인 상태를 확인하여 상태 초기화
     useEffect(() => {
         const storedLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-        setIsLoggedIn(storedLoggedIn);
-    }, []);
-
-    // 로그인 상태가 변경될 때 로컬 스토리지에 반영
-    useEffect(() => {
-        localStorage.setItem("isLoggedIn", isLoggedIn);
-    }, [isLoggedIn]);
+        if (!storedLoggedIn) {
+            dispatch(logoutUser());
+        }
+    }, [dispatch]);
 
     return (
         <Router>
             <div className="App">
                 <Routes>
+                    {/* 기본 페이지를 /login으로 설정 */}
                     <Route
                         path="/"
                         element={<Navigate replace to="/login" />}
                     />
+                    <Route path="/login" element={<LoginPage />} />
                     <Route
-                        path="/login"
-                        element={<LoginPage setIsLoggedIn={setIsLoggedIn} />}
+                        path="/waiting"
+                        element={
+                            isLoggedIn ? (
+                                <WaitingRoomPage />
+                            ) : (
+                                <Navigate replace to="/login" />
+                            )
+                        }
                     />
-                    <Route path="/waiting" element={<WaitingRoomPage />} />
                     <Route path="/game" element={<GameContainer />} />
                     <Route path="/tetris" element={<GamePage />} />
                 </Routes>
