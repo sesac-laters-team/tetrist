4570d381d36fbd933e4b2c27a435be0b8ddb83db
yzlybe <yzlybe@gmail.com>
2024-05-01 20:35:39 +0900

    feat:rooms
    ### controller/Cmain
    - 방 생성 시 클라이언트에서 받아오는 변수 명 변경,  클라이언트에 전달하는 정보 userId, roomId 추가
    - params에 roomId로 조회 시 해당 방, 방 생성 유저, 방 참여 유저 정보 조회 기능 추가
    - 생성된 방에 게스트 유저 입장 기능 추가
    - 게스트 유저 퇴장 시 방 정보에서 게스트 정보 제거 기능 추가
    
    ### routes/router
    - 추가된 라우터 및 스웨거 정보 추가
    
    ### models/Rooms
    - rooms 테이블에 guest_id 항목 추가
    
    ### sql/rooms
    - 수정된 테이블에 맞추어 로컬 데이터 sql 수정

--- server/controller/Cmain.js
+++ server/controller/Cmain.js
@@ -80,6 +80,8 @@ exports.rank = async (req, res) => {
     }
 };
 
+// room
+
 // GET /api-server/rooms
 // 방 목록 전체 조회
 exports.roomsList = async (req, res) => {
@@ -96,14 +98,162 @@ exports.roomsList = async (req, res) => {
 // 방 추가
 exports.postRoom = async (req, res) => {
     try {
-        const { name, password } = req.body;
-        console.log(req.session.userId, name, password);
-        await roomsModel.create({
+        const { r_name, r_password } = req.body;
+        const createRoom = await roomsModel.create({
             user_id: req.session.userId,
-            r_name: name,
-            r_password: password,
+            r_name: r_name,
+            r_password: r_password,
         });
-        res.send({ result: true });
+        res.send({
+            result: true,
+            userId: createRoom.user_id,
+            roomId: createRoom.room_id,
+        });
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).send("server error");
+    }
+};
+
+// GET /api-server/room/:roomId
+// 특정 방 데이터 받아오기
+exports.roomData = async (req, res) => {
+    try {
+        const { roomId } = req.params;
+        const room = await roomsModel.findOne({
+            where: {
+                room_id: roomId,
+            },
+        });
+        const creator = await usersModel.findOne({
+            where: {
+                user_id: room.user_id,
+            },
+        });
+        const guest = await usersModel.findOne({
+            where: {
+                user_id: room.guest_id,
+            },
+        });
+
+        if (room) {
+            res.status(200).send({
+                result: true,
+                roomData: room,
+                creatorData: creator,
+                guestData: guest,
+                msg: "룸 정보 확인",
+            });
+        } else {
+            res.status(404).send({
+                result: false,
+                msg: "룸 정보를 찾을 수 없습니다.",
+            });
+        }
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).send("server error");
+    }
+};
+
+// POST /api-server/room/enter/:roomId
+// 만들어진 방에 다른 유저가 입장 요청
+exports.enterRoom = async (req, res) => {
+    try {
+        const { roomId } = req.params;
+        const { r_password } = req.body;
+
+        const findRoom = await roomsModel.findOne({
+            where: {
+                room_id: roomId,
+            },
+        });
+
+        // 공개방인 경우
+        if (!findRoom.r_password) {
+            if (!findRoom.guest_id) {
+                // 입장 성공
+                await roomsModel.update(
+                    { guest_id: req.session.userId },
+                    { where: { room_id: roomId } }
+                );
+                res.send({
+                    result: true,
+                    guestId: req.session.userId,
+                });
+            } else {
+                // 방에 guest_id가 이미 존재
+                res.status(409).send({
+                    result: false,
+                    msg: "인원이 가득 찼습니다. 입장할 수 없습니다.",
+                });
+            }
+            // 비밀방인 경우
+        } else {
+            if (r_password === findRoom.r_password) {
+                // 비밀번호 일치
+                if (!findRoom.guest_id) {
+                    // 입장 성공
+                    await roomsModel.update(
+                        { guest_id: req.session.userId },
+                        { where: { room_id: roomId } }
+                    );
+                    res.send({
+                        result: true,
+                        guestId: req.session.userId,
+                    });
+                } else {
+                    // 방에 guest_id가 이미 존재
+                    res.status(409).send({
+                        result: false,
+                        msg: "인원이 가득 찼습니다. 입장할 수 없습니다.",
+                    });
+                }
+            } else {
+                // 비밀번호 불일치
+                res.status(401).send({
+                    result: false,
+                    msg: "비밀번호가 일치하지 않습니다.",
+                });
+            }
+        }
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).send("server error");
+    }
+};
+
+// POST /api-server/room/leave/:roomId
+// 만들어진 방에 입장했던 다른 유저가 퇴장
+exports.leaveRoom = async (req, res) => {
+    try {
+        const { roomId } = req.params;
+
+        await roomsModel.update(
+            { guest_id: null },
+            { where: { room_id: roomId } }
+        );
+        res.send({ result: true, roomId: roomId });
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).send("server error");
+    }
+};
+
+// PATCH /api-server/room/:roomId
+// 방 게임 상태 전환
+exports.patchRoom = async (req, res) => {
+    try {
+        const { roomId } = req.params;
+        const [isUpdated] = await roomsModel.update(
+            { r_status: sequelize.literal("NOT r_status") }, // 현재값과 반대
+            { where: { room_id: roomId } }
+        );
+        isUpdated
+            ? res.status(200).send({ result: true, roomId })
+            : res
+                  .status(404)
+                  .send({ result: false, msg: "존재하지 않는 방입니다." });
     } catch (error) {
         console.log("error", error);
         res.status(500).send("server error");
