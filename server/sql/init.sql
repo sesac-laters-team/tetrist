1b190bcdb229642aa6bf256aa192f2d63d09cab4
hyein310 <kanghyein310@naver.com>
2024-05-01 15:38:59 +0900

    fix: 코드 통합 및 오류 수정

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -1,5 +1,8 @@
--- Active: 1707101713117@@127.0.0.1@3306@sesac
+-- Active: 1707101283581@@127.0.0.1@3306@tetrist
 show databases;
+
 create database tetrist character set utf8mb4 collate utf8mb4_unicode_ci;
+
 use tetrist;
+
 show tables;
