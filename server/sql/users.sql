580335e1a4a316a7e72065518a0746d56099bd96
yzlybe <yzlybe@gmail.com>
2024-05-06 17:15:33 +0900

    fix: custom
    - 유저 테이블 수정에 따라 모델 수정
    - 커스텀 기능 변경

--- server/sql/users.sql
+++ server/sql/users.sql
@@ -1,4 +1,4 @@
--- Active: 1707101283311@@127.0.0.1@3306@tetris
+-- Active: 1707101281051@@127.0.0.1@3306@tetrist
 desc users;
 
 show tables;
@@ -6,32 +6,32 @@ select * from users;
 
 INSERT INTO
     users (
-        user_id, email, password, nickname, point, custom, win, lose, rating, connecting, chat_penalty, access_penalty
+        user_id, email, password, nickname,  win, lose, rating, access_penalty
     )
 VALUES (
-        1, "user1@test.com", "user1234!", "유저1", 1000, '{"profile":1, "profileEdge":2, "theme":3}', 1, 3, 100, true, true, false
+        1, "user1@test.com", "user1234!", "유저1", 1, 3, 100, false
     );
 
 INSERT INTO
     users (
-        user_id, email, password, nickname, point, custom, win, lose, rating, connecting, chat_penalty, access_penalty
+        user_id, email, password, nickname, win, lose, rating, access_penalty
     )
 VALUES (
-        2, "user2@test.com", "user1234", "유저2", 2000, '{"profile":1, "profileEdge":2, "theme":3}', 0, 0, 0, false, false, true
+        2, "user2@test.com", "user1234", "유저2", 0, 0, 0, true
     );
 
 INSERT INTO
     users (
-        user_id, email, password, nickname, point, custom, win, lose, rating, connecting, chat_penalty, access_penalty
+        user_id, email, password, nickname, win, lose, rating, access_penalty
     )
 VALUES (
-        3, "user3@test.com", "user3pw", "유저3", 3000, '{"profile":1, "profileEdge":2, "theme":3}', 10, 2, 500, true, false, false
+        3, "user3@test.com", "user3pw", "유저3", 10, 2, 500, false
     );
 
 INSERT INTO
     users (
-        user_id, email, password, nickname, point, custom, win, lose, rating, connecting, chat_penalty, access_penalty
+        user_id, email, password, nickname, win, lose, rating, access_penalty
     )
 VALUES (
-        4, "user4@test.com", "user4pw", "유저4", 4000, '{"profile":1, "profileEdge":2, "theme":3}', 100, 100, 700, false, false, false
+        4, "user4@test.com", "user4pw", "유저4", 100, 100, 700, false
     );
